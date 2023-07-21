849ef221d26abb3430b992beb82153ff91882e09
Kabir <kabirvirk51@gmail.com>
2023-07-21 01:32:15 -0400

    Added functionality of changing the board to store more pixels by using the slider

--- script.js
+++ script.js
@@ -1,62 +1,72 @@
 const container = document.querySelector("#container");
-
-let divs = [];
-
-for (let i = 0; i < 256; i++) {
-  let gridDivs = document.createElement("div");
-  gridDivs.classList.add("squares");
-
-  divs[i] = gridDivs;
-}
-
-for (let i = 0; i < 256; i++) {
-  container.appendChild(divs[i]);
-}
-
-let isColor = false;
-
-for (let i = 0; i < 256; i++) {
-  divs[i].addEventListener("mouseenter", () => {
-    if (isColor == true) {
-      divs[i].style.backgroundColor = "black";
-    }
-  });
-
-  divs[i].addEventListener("mousedown", () => {
-    isColor = true;
-    divs[i].style.backgroundColor = "black";
-  });
-
-  divs[i].addEventListener("mouseup", () => {
-    isColor = false;
-  });
-}
-
+const clearContainer = document.querySelector("#outer-container");
 const sizeSlider = document.getElementById("sizeSlider");
 const sizeSliderText = document.getElementById("sizeSliderText");
 
-for (let i = 0; i < 100; i++) {
-  if (sizeSlider.value == i) {
-    sizeSlider.addEventListener("input", () => {
-      sizeSliderText.textContent = sizeSlider.value + " x " + sizeSlider.value;
+let divs = [];
+let gridDivs;
+let tempDivs;
+let sizeSliderValue = 16;
+let isColor = false;
+
+adjustSquareSizes();
+setupEventListeners();
+
+sizeSlider.addEventListener("input", () => {
+  sizeSliderValue = sizeSlider.value;
+  sizeSliderText.textContent = sizeSlider.value + " x " + sizeSlider.value;
+  for (let i = 0; i < tempDivs; i++) {
+    divs[i].style.backgroundColor = "white";
+  }
+  adjustSquareSizes();
+  setupEventListeners();
+});
+
+function adjustSquareSizes() {
+  tempDivs = sizeSliderValue * sizeSliderValue;
+  divs = []; // Clear the existing divs array
+
+  for (let i = 0; i < tempDivs; i++) {
+    gridDivs = document.createElement("div");
+    gridDivs.classList.add("squares");
+    divs.push(gridDivs);
+  }
+
+  container.innerHTML = ""; // Clear the existing content
+  for (let i = 0; i < tempDivs; i++) {
+    divs[i].style.width = `calc(100% / ${sizeSliderValue})`;
+    divs[i].style.height = `calc(100% / ${sizeSliderValue})`;
+    container.appendChild(divs[i]);
+  }
+  container.style.cssText = "overflow:hidden";
+}
+
+function setupEventListeners() {
+  for (let i = 0; i < tempDivs; i++) {
+    divs[i].addEventListener("mouseenter", () => {
+      if (isColor == true) {
+        divs[i].style.backgroundColor = "black";
+      }
     });
-  } else {
-    continue;
+
+    divs[i].addEventListener("mousedown", () => {
+      isColor = true;
+      divs[i].style.backgroundColor = "black";
+    });
+
+    divs[i].addEventListener("mouseup", () => {
+      isColor = false;
+    });
   }
 }
 
-const clearContainer = document.querySelector("#outer-container");
-
-let clear = document.createElement("button");
-
+const clear = document.createElement("button");
 clear.classList.add("clear");
-
 clear.textContent = "Clear";
-
 clearContainer.appendChild(clear);
 
 clear.addEventListener("click", () => {
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < tempDivs; i++) {
     divs[i].style.backgroundColor = "white";
   }
 });
